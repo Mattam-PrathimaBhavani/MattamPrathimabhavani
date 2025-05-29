a011e3ee1e048dac75750e238f13708e8ec68c01
Mattam-PrathimaBhavani <113002634+Mattam-PrathimaBhavani@users.noreply.github.com>
2025-05-29 09:43:44 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,22 @@
+# # Read the file
+# with open("example.txt", "r") as file:
+#     print("Before appending:")
+#     print(file.read())
+
+# # Append a line
+# with open("example.txt", "a+") as file:
+#     file.write("\nAppended line: Hello from Python!")
+
+
+#with open("example.txt", "r") as file:
+
+    #print("\nAfter appending:")
+    #print(file.read())
+
+
+#wirte the file
+
+#file = open("example.txt", "w")
+#file.write("Hello, World!")
+#file.close()
+#print ("File opened successfully!!")
